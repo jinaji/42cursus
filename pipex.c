aea450dacb106f0d32ab7766a3583797fbbb3b88
jina <jinkim2@student.42seoul.kr>
2022-06-16 21:57:21 +0900

    rr

---
+++ pipex.c
@@ -0,0 +1,8 @@
+#include "pipex.h"
+
+int	main(int ac, char **av, char **envp)
+{
+	if (ac != 5)
+		return (0);
+	
+}
---
+++ pipex.h
@@ -0,0 +1,8 @@
+#ifndef PIPEX_H
+# define PIPEX_H
+
+typedef struct s_cmd{
+	char	*path;
+}				t_cmd;
+
+#endif
