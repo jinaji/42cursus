8416a4eb51e774ad1b63e113066fd6d4a516651d
Jina Kim <jinkim2@c7r1s7.42seoul.kr>
2022-10-18 21:04:20 +0900

    ???

---
+++ cpp01/ex00/Zombie.cpp
@@ -0,0 +1,46 @@
+#include "Zombie.hpp"
+
+void	Zombie::announce(void)
+{
+ 	std::cout << name << ": BraiiiiiiinnnzzzZ..." << std::endl;
+}
+
+Zombie::Zombie()
+{
+	std::cout << name << " created" << std::endl;
+}
+
+Zombie::Zombie(std::string name)
+{
+	this->name = name;
+	std::cout << name << " created" << std::endl;
+}
+
+Zombie::~Zombie()
+{
+	std::cout << name << " destructed" << std::endl;
+}
+
+Zombie*	newZombie(std::string name)
+{
+	return (new Zombie(name));
+}
+
+void	randomChump(std::string name)
+{
+	Zombie	tmp;
+
+	tmp = Zombie(name);
+	tmp.announce();
+}
+
+int main()
+{
+	Zombie	*a;
+	Zombie	b;
+
+	a = newZombie("a");
+	Zombie("b").announce();
+	a->announce();
+	delete a;
+}
