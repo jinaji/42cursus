3effde5af31f000767e47d383f601cc464328dec
Jina Kim <jinkim2@c4r3s4.42seoul.kr>
2022-11-19 21:04:21 +0900

    04??

--- cpp04/ex01/Animal.cpp
+++ cpp04/ex01/Animal.cpp
@@ -22,10 +22,10 @@ Animal::~Animal()
 	std::cout << "Animal default destructor called" << std::endl;
 }
 
-// void	Animal::makeSound() const
-// {
-// 	std::cout << "an appropriate Animal sound" << std::endl;
-// }
+void	Animal::makeSound() const
+{
+	std::cout << "an appropriate Animal sound" << std::endl;
+}
 
 std::string	Animal::getType() const
 {
