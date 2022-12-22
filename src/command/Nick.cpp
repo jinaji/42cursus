bb26f72d3b1b6ed854fb5de2e59645260eb30c37
gyuminv2 <inamemin3@gmail.com>
2022-12-23 02:15:47 +0900

    feat: NICK

--- src/command/Nick.cpp
+++ src/command/Nick.cpp
@@ -14,12 +14,18 @@ void    Command::Nick()
         }
     }
     // 유효한 별명 X (숫자) -> 432
-    // if (isdigit(_parsingPara[0].at(0) == true))
-    //     this->Numerics(432);
+    if (isdigit(_parsingPara[0].at(0) == true))
+    {
+        this->Numerics(432);
+        return ;
+    }
     
     // 매개변수 X -> 431
-    // if (_paraNum == 0)
-    //     this->Numerics(431);
+    if (_paraNum == 0)
+    {
+        this->Numerics(431);
+        return ;
+    }
 
     _caller.setNick(_parsingPara[0]);
     
